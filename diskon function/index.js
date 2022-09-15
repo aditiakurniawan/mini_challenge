023ef72fa06dcf7749b4aa19695ff50eadf1eef2
aditiakurniawan <108774796+aditiakurniawan@users.noreply.github.com>
2022-09-15 20:18:22 +0700

    function voucher discount

---
+++ diskon function/index.js
@@ -0,0 +1,39 @@
+let belanja;
+let voucher;
+let diskon;
+let potongan;
+let total;
+
+function hitungVoucher(voucher, belanja) {
+  if (voucher == "DumbMerchMurahBanget") {
+    potongan = belanja * 0.5;
+    if (potongan > 45000) {
+      potongan = 45000;
+    }
+  } else if (voucher == "DumbWMerchBerkah") {
+    potongan = belanja * 0.25;
+    if (potongan > 20000) {
+      potongan = 20000;
+    }
+  } else {
+    potongan = 0;
+  }
+
+  if (voucher == "DumbWMerchBerkah") {
+    diskon = "25%";
+  } else if (voucher == "DumbMerchMurahBanget") {
+    diskon = "50%";
+  } else {
+    diskon = "";
+  }
+
+  total = belanja - potongan;
+  console.log("Total Belanja    : Rp.", belanja);
+  console.log("Diskon : ", diskon);
+  console.log("Potongan         : Rp.", potongan);
+  console.log("---------------------------------");
+  console.log("Total Pembayaran : Rp.", total);
+}
+
+hitungVoucher("DumbMerchMurahBanget", 100000);
+// hitungVoucher("DumbWMerchBerkah", 100000);
