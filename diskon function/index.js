9524ed63867e48baf47fccb210a2487cec273180
aditiakurniawan <108774796+aditiakurniawan@users.noreply.github.com>
2022-09-15 23:03:57 +0700

    finising

--- diskon function/index.js
+++ diskon function/index.js
@@ -5,35 +5,41 @@ let potongan;
 let total;
 
 function hitungVoucher(voucher, belanja) {
-  if (voucher == "DumbMerchMurahBanget") {
-    potongan = belanja * 0.5;
-    if (potongan > 45000) {
-      potongan = 45000;
-    }
-  } else if (voucher == "DumbWMerchBerkah") {
-    potongan = belanja * 0.25;
-    if (potongan > 20000) {
-      potongan = 20000;
-    }
-  } else {
+  if (belanja < 50000) {
     potongan = 0;
-  }
-
-  if (voucher == "DumbWMerchBerkah") {
-    diskon = "25%";
-  } else if (voucher == "DumbMerchMurahBanget") {
-    diskon = "50%";
   } else {
-    diskon = "";
+    if (voucher == "DumbMerchMurahBanget") {
+      potongan = belanja * 0.5;
+      if (potongan > 45000) {
+        potongan = 45000;
+      }
+    } else if (voucher == "DumbWMerchBerkah") {
+      potongan = belanja * 0.25;
+      if (potongan > 20000) {
+        potongan = 20000;
+      }
+    } else {
+      potongan = 0;
+    }
   }
-
+  if (belanja < 50000) {
+    voucher = "";
+  } else {
+    if (voucher == "DumbWMerchBerkah") {
+      diskon = "25%";
+    } else if (voucher == "DumbMerchMurahBanget") {
+      diskon = "50%";
+    } else {
+      diskon = "";
+    }
+  }
   total = belanja - potongan;
   console.log("Total Belanja    : Rp.", belanja);
   console.log("Diskon : ", diskon);
   console.log("Potongan         : Rp.", potongan);
-  console.log("---------------------------------");
+  console.log("------------------------------  (-)");
   console.log("Total Pembayaran : Rp.", total);
 }
 
-hitungVoucher("DumbMerchMurahBanget", 100000);
-// hitungVoucher("DumbWMerchBerkah", 100000);
+// hitungVoucher("DumbMerchMurahBanget", 100000);
+hitungVoucher("DumbWMerchBerkah", 100000);
